f1fd0c7943da055a36cee0d12800bc8d5af4a499
martinkalaydzhiev-taulia <87652109+martinkalaydzhiev-taulia@users.noreply.github.com>
2021-09-07 11:16:16 +0300

    jpa demo

---
+++ src/main/java/com/example/demo/model/DriverRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface DriverRepository extends CrudRepository<Driver, Long> {
+    @Override
+    Iterable<Driver> findAll();
+
+    Iterable<Driver> findAllByFirstName(String name);
+    Iterable<Driver> findAllByLastName(String name);
+    Iterable<Driver> findDriversByWinsBefore(int n);
+    Iterable<Driver> findDriversByWinsAfter(int n);
+    Iterable<Driver> findFirstByOrderByWinsDesc();
+    Iterable<Driver> findFirstByOrderByPointsDesc();
+    Iterable<Driver> findDriversByWinsBetween(int from, int to);
+    Iterable<Driver> findAllByOrderByWinsDesc();
+    Iterable<Driver> findAllByOrderByPointsDesc();
+}
